583767baf8fc57ebc6392d906a718e833a332034
isaDevs <isabel.calzadilla.18@gmail.com>
2022-06-18 09:36:36 +0200

    1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("Array");
+
+        int [] numbers = {1, 2, 3};
+        System.out.println(numbers[0]);
+
+        for(int i : numbers) {
+            System.out.println(i);
+ /       }
+    }
+}
